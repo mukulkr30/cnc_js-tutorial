57bb737c4793cc578ab89f8996b1d63c95baf175
mukulkr30 <mukul.23b1531040@abes.ac.in>
2025-08-30 10:57:18 +0000

    new commit

---
+++ d0_basic/01_variable.js
@@ -0,0 +1,10 @@
+const accId = 23    //not allowed const variable doesnot change
+let accEmail = "adg.com"  //can change
+var accPass = "ffgchhd"   //prefer not to use var because issue in scope
+let accState;     //if not assign value in variable it data type is undefined
+
+city = "jaipur"
+// accId = 30    
+accEamil = "ddd.com"    
+console.log(accEmail)
+console.table([accId,accEmail,accPass,city])
---
+++ d0_basic/02_datatype.js
@@ -0,0 +1,15 @@
+"use strict";     //treat all JS code as newer version
+
+//datatypes
+
+let name = "mukul"   //string
+let age = 20   //number    2^53 range    for more then range use bigint
+
+let male = true   //bool
+// null ==>standalone value and datatype as well ,it can be assign
+//undefined  ==> means no assign value
+//symbol ==> this data type used to defiine unique val
+//object 
+
+
+console.log(typeof null)
