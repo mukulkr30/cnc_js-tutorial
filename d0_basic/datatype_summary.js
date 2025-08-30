cbd271230a1e3f6caff29c9a5c93722923c3c46d
mukulkr30 <mukul.23b1531040@abes.ac.in>
2025-08-30 14:37:29 +0000

    stack n heap memory

--- d0_basic/datatype_summary.js
+++ d0_basic/datatype_summary.js
@@ -11,3 +11,24 @@ const obj = {
 }
 console.log(ar)
 console.log(obj)
+
+
+//  memory in js
+// stack(store premitive data type)   not change at origional place  
+// heap(store non-premitive data type) change at origional place
+
+let a = 2
+let y = a
+y = 3
+console.log(a)    //changing in y don't change the value in a
+console.log(y)
+
+let z = {
+    name :"mukul",
+    age: 20,
+}
+let x = z
+x.age = 21
+
+console.log(z.age)    //changing in x change the value in z because both reference the same memory at heap
+console.log(x.age)
