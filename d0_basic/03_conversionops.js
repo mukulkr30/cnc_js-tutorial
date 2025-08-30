6e2a4e5571099a04c13fddeb8b0a728f4b38d9be
mukulkr30 <mukul.23b1531040@abes.ac.in>
2025-08-30 14:09:44 +0000

    revise the old

--- d0_basic/03_conversionops.js
+++ d0_basic/03_conversionops.js
@@ -7,13 +7,13 @@ let tonum = Number(sc)
 let a = "33abe"   //if we convert in number this covnvert iin NaN
 let b = null      //if convert in number gives 0
 let x = true      // convert in 1 for false it is 0
-console.log(typeof score)
-console.log(typeof sc)
-console.log(typeof tonum)
+// console.log(typeof score)
+// console.log(typeof sc)
+// console.log(typeof tonum)
 let tonum1 = Number(a)
 let tonum2 = Number(b)
-console.log(typeof tonum1)
-console.log(typeof tonum2)
+// console.log(typeof tonum1)
+// console.log(typeof tonum2)
 
 
 // "33"==>33
@@ -23,8 +23,37 @@ console.log(typeof tonum2)
 
 let q = 1
 let convInbool = Boolean(q)
-console.log(convInbool)
+// console.log(convInbool)
 
 //convert string in bool
 // empty string ==> false
 ///filled string ==> true
+
+
+
+// operations 
+
+let v = 4
+let negv = -v   //if i put minus before var it gives negative value
+
+// concatenate string by adding +
+let a1 = "mukul"
+let a2 = " kumar"
+
+let a3 = a1+a2
+// console.log(a3)
+
+
+// what if there a string and number adds
+// console.log("1"+1)    // =11       after string all number converted in string
+// console.log(1+"1")    // =11
+// console.log("1"+1+1)    // =111
+// console.log(1+1+"1")    // =21
+// console.log(+true)      // = 1
+
+
+// comperision string and number
+
+
+console.log("2">1)    
+console.log("02">1)
---
+++ d0_basic/datatype_summary.js
@@ -0,0 +1,13 @@
+// how data type stored in memory 
+
+// 1 premitive (string ,number,boolean,null,undefined,NaN,symbol)->7   alsoo bigint
+// 2 reference or non premitive (array, objects,functions)
+
+const ar = [1,2,3,4,5]
+
+const obj = {
+    name:"mukul",
+    age:23,
+}
+console.log(ar)
+console.log(obj)
